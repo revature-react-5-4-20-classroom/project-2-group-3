1d881cdc9b8a43c16d051b83216979f1be561ab4
joel joseph <joeljva23@gmail.com>
2020-06-17 10:06:46 -0400

    jservice

--- java-backend/src/main/java/com/group3/project2/repositories/AppointmentRepository.java
+++ java-backend/src/main/java/com/group3/project2/repositories/AppointmentRepository.java
@@ -1,8 +1,19 @@
 package com.group3.project2.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import com.group3.project2.models.Appointment;
+import com.group3.project2.models.Patient;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
+  
+  
+
+  
+  @Query("select u from Appointment u where u.patient.patientId=:id")
+  List<Appointment> findByPatientIdJ(Integer id);
+  
+  
 
 }
